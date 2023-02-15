6929f03883794ae2efdff81eef42407830b158b1
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-02-15 15:13:58 +0700

    first commit

--- src/pages/signin/index.js
+++ src/pages/signin/index.js
@@ -1,1 +1,39 @@
-rc;
+import React from "react";
+import { Container, Card } from "react-bootstrap";
+
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
+function PageSignin() {
+  return (
+    <Container>
+      <Card style={{ width: "18rem" }}>
+        <Card.Title>Form login</Card.Title>
+        <Card.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+              <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicCheckbox">
+              <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+    </Container>
+  );
+}
+
+export default PageSignin;
