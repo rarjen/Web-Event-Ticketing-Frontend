99e27a5b2391db4a4d0e22a1adebb720b03ab09a
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-02-16 11:09:42 +0700

    Import button from react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Button } from "react-bootstrap";
 import PageSignin from "./pages/signin";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<>Home</>}></Route>
+        <Route path="/" element={<Button>Home</Button>}></Route>
         <Route path="/signin" element={<PageSignin />}></Route>
       </Routes>
     </BrowserRouter>
