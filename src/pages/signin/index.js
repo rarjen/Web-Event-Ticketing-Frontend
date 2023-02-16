bd14c06a4ef9d4b562d03d3cfe85740a85f5fa4f
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-02-16 15:09:36 +0700

    fix page signin

--- src/pages/signin/index.js
+++ src/pages/signin/index.js
@@ -1,25 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Card } from "react-bootstrap";
 import { Form } from "react-bootstrap";
 import PButton from "../../components/Button";
+import TextInputWithLabel from "../../components/TextInputWithLabel";
+import axios from "axios";
+import PAlert from "../../components/Alert";
 
 function PageSignin() {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const res = await axios.post("http://localhost:9000/api/v1/auth/signin", {
+        email: form.email,
+        password: form.password,
+      });
+
+      console.log(res);
+    } catch (error) {
+      console.log(error.response.data.message);
+    }
+  };
+
   return (
     <Container md={12}>
+      <PAlert message="Test" type="danger" />
       <Card style={{ width: "50%" }} className="m-auto mt-5 p-2">
         <Card.Title className="text-center">Form Sign In</Card.Title>
         <Card.Body>
           <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-
-            <PButton variant="primary">Submit</PButton>
+            <TextInputWithLabel
+              label="Email address"
+              name="email"
+              value={form.email}
+              type="email"
+              placeholder="Enter email"
+              onChange={handleChange}
+            />
+            <TextInputWithLabel
+              label="Password"
+              name="password"
+              value={form.password}
+              type="password"
+              placeholder="Password"
+              onChange={handleChange}
+            />
+            <PButton action={handleSubmit} variant="primary">
+              Submit
+            </PButton>
           </Form>
         </Card.Body>
       </Card>
