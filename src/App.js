fc74b06c36b275212b57e70c241a1c3a62c3f97b
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-02-17 13:10:29 +0700

    import dashboard router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Button } from "react-bootstrap";
-import PageSignin from "./pages/signin";
+import SigninPage from "./pages/signin";
+import DashboardPage from "./pages/dashboard";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Button>Home</Button>}></Route>
-        <Route path="/signin" element={<PageSignin />}></Route>
+        <Route path="/signin" element={<SigninPage />}></Route>
+        <Route path="/dashboard" element={<DashboardPage />}></Route>
       </Routes>
     </BrowserRouter>
   );
