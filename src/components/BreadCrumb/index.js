75ef6e286a502098bc2481d7f0ddcc6a50bac716
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-02-17 14:43:49 +0700

    create BreadCrumb components for dashboard

---
+++ src/components/BreadCrumb/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Breadcrumb } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+
+function PBreadCrumb({ textSecond, textThird, urlSecond }) {
+  const navigate = useNavigate;
+  return (
+    <Breadcrumb className="my-2">
+      <Breadcrumb.Item onClick={() => navigate("/")}>Home</Breadcrumb.Item>
+      {!textThird && <Breadcrumb.Item active>{textSecond}</Breadcrumb.Item>}
+      {textThird && (
+        <Breadcrumb.Item onClick={() => navigate(urlSecond)}>
+          {textSecond}
+        </Breadcrumb.Item>
+      )}
+      {!textThird && <Breadcrumb.Item active>{textThird}</Breadcrumb.Item>}
+    </Breadcrumb>
+  );
+}
+
+export default PBreadCrumb;
