79c022b2392875fe4ebda9fb62002e7a5659ff1b
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-02-17 13:11:32 +0700

    use form signin

--- src/pages/signin/index.js
+++ src/pages/signin/index.js
@@ -1,62 +1,71 @@
 import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { Container, Card } from "react-bootstrap";
-import { Form } from "react-bootstrap";
-import PButton from "../../components/Button";
-import TextInputWithLabel from "../../components/TextInputWithLabel";
 import axios from "axios";
 import PAlert from "../../components/Alert";
+import { config } from "../../configs";
+import FormSignin from "./form";
 
 function PageSignin() {
+  const navigate = useNavigate();
+
+  // States
   const [form, setForm] = useState({
     email: "",
     password: "",
   });
 
+  const [alert, setAlert] = useState({
+    status: false,
+    message: "",
+    type: "danger",
+  });
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  // Functions
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
+    setAlert({ status: false });
   };
 
   const handleSubmit = async () => {
     try {
-      const res = await axios.post("http://localhost:9000/api/v1/auth/signin", {
-        email: form.email,
-        password: form.password,
-      });
+      setIsLoading(true);
+      const res = await axios.post(
+        `${config.api_host_dev}/cms/auth/signin`,
+        form
+      );
 
-      console.log(res);
+      console.log(res.data.data.token);
+
+      setIsLoading(false);
+      navigate("/dashboard");
     } catch (error) {
-      console.log(error.response.data.message);
+      setIsLoading(false);
+      setAlert({
+        status: true,
+        message: error?.response?.data?.message ?? "Internal Server Error!",
+        type: "danger",
+      });
     }
   };
 
   return (
-    <Container md={12}>
-      <PAlert message="Test" type="danger" />
+    <Container md={12} className="my-5">
+      <div className="m-auto" style={{ width: "50%" }}>
+        {/* Jika alert true akan menampilkan alert component*/}
+        {alert.status && <PAlert type={alert.type} message={alert.message} />}
+      </div>
       <Card style={{ width: "50%" }} className="m-auto mt-5 p-2">
         <Card.Title className="text-center">Form Sign In</Card.Title>
-        <Card.Body>
-          <Form>
-            <TextInputWithLabel
-              label="Email address"
-              name="email"
-              value={form.email}
-              type="email"
-              placeholder="Enter email"
-              onChange={handleChange}
-            />
-            <TextInputWithLabel
-              label="Password"
-              name="password"
-              value={form.password}
-              type="password"
-              placeholder="Password"
-              onChange={handleChange}
-            />
-            <PButton action={handleSubmit} variant="primary">
-              Submit
-            </PButton>
-          </Form>
-        </Card.Body>
+        <FormSignin
+          form={form}
+          handleChange={handleChange}
+          handleSubmit={handleSubmit}
+          isLoading={isLoading}
+        />
+        <Card.Body></Card.Body>
       </Card>
     </Container>
   );
