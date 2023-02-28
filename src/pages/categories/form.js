9f2d9e5833a813d99fbe92f5de56dd9f59a364da
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-02-28 17:18:21 +0700

    make CategoriesForm

---
+++ src/pages/categories/form.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Form } from "react-bootstrap";
+import PButton from "../../components/Button";
+import PTextInputWithLabel from "../../components/TextInputWithLabel";
+
+export default function CategoriesForm({
+  handleSubmit,
+  form,
+  handleChange,
+  isLoading,
+  edit,
+}) {
+  <Form>
+    <PTextInputWithLabel
+      placeholder={"Masukkan nama kategori"}
+      label={"Nama Kategori"}
+      name="name"
+      value={form.name}
+      type="text"
+      onChange={handleChange}
+    />
+    <PButton variant="primary" action={handleSubmit} loading={isLoading}>
+      {edit ? "Ubah" : "Simpan"}
+    </PButton>
+  </Form>;
+}
