f688a2aec6d39cb1ad81b0f53279b7ba4738fed1
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-03-02 16:11:31 +0700

    fix typo

--- src/components/TbodyWithAction/index.js
+++ src/components/TbodyWithAction/index.js
@@ -51,7 +51,7 @@ export default function TbodyWithAction({
               )}
               {!actionNotDisplay && (
                 <td>
-                  {customAction && customAction(data._id, data.statusEbent)}
+                  {customAction && customAction(data._id, data.statusEvent)}
                   {editUrl && (
                     <PButton
                       variant={success}
