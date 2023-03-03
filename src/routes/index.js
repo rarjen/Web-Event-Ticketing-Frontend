4f6f13a05117bc682164d17bc76fca37bb0dbdc0
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-03-03 19:53:25 +0700

    Import Talent routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ import GuestOnlyRoute from "../components/GuestOnlyRoute";
 import Login from "../pages/signin";
 import { HomeRoute } from "./HomeRoute";
 import { CategoriesRoute } from "./CategoriesRoute";
+import { TalentsRoute } from "./TalentsRoute";
 import PNavbar from "../components/Navbar";
 
 export function AppRoutes() {
@@ -30,6 +31,7 @@ export function AppRoutes() {
       >
         <Route path="dashboard/*" element={<HomeRoute />} />
         <Route path="categories/*" element={<CategoriesRoute />} />
+        <Route path="talents/*" element={<CategoriesRoute />} />
         <Route path="/" element={<Navigate to="/dashboard" replace={true} />} />
       </Route>
     </Routes>
