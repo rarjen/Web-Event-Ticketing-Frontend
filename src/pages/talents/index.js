9905c13c3d0d053f55163abbf06f24e84d51c95f
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-03-06 10:35:25 +0700

    create page talents

--- src/pages/talents/index.js
+++ src/pages/talents/index.js
@@ -1,9 +1,104 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Container } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import PBreadCrumb from "../../components/BreadCrumb";
+import PButton from "../../components/Button";
+import PTableWithAction from "../../components/TableWithAction";
+import PSearchInput from "../../components/SearchInput";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchTalents, setKeyword } from "../../redux/talents/actions";
+import PAlert from "../../components/Alert";
+import Swal from "sweetalert2";
+import { deleteData } from "../../utils/fetch";
+import { setNotif } from "../../redux/notif/actions";
+import { accessTalents } from "../../const/access";
 
-export default function TalentsPage() {
+function TalentsPage() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const notif = useSelector((state) => state.notif);
+  const talents = useSelector((state) => state.talents);
+
+  const [access, setAccess] = useState({
+    tambah: false,
+    hapus: false,
+    edit: false,
+  });
+
+  const checkAccess = () => {
+    let { role } = localStorage.getItem("auth")
+      ? JSON.parse(localStorage.getItem("auth"))
+      : {};
+    const access = { tambah: false, hapus: false, edit: false };
+    Object.keys(accessTalents).forEach(function (key, index) {
+      if (accessTalents[key].indexOf(role) >= 0) {
+        access[key] = true;
+      }
+    });
+    setAccess(access);
+  };
+
+  useEffect(() => {
+    checkAccess();
+  }, []);
+
+  useEffect(() => {
+    dispatch(fetchTalents());
+  }, [dispatch, talents.keyword]);
+
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Apa kamu yakin?",
+      text: "Anda tidak akan dapat mengembalikan ini!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Iya, Hapus",
+      cancelButtonText: "Batal",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        const res = await deleteData(`/cms/talents/${id}`);
+
+        dispatch(
+          setNotif(
+            true,
+            "success",
+            `berhasil hapus speaker ${res.data.data.name}`
+          )
+        );
+
+        dispatch(fetchTalents());
+      }
+    });
+  };
+
   return (
-    <div>
-      <h1>Mantap</h1>
-    </div>
+    <Container className="mt-3">
+      <PBreadCrumb textSecound={"Talents"} />
+      {access.tambah && (
+        <div className="mb-3">
+          <PButton action={() => navigate("/talents/create")}>Tambah</PButton>
+        </div>
+      )}
+      <PSearchInput
+        query={talents.keyword}
+        handleChange={(e) => dispatch(setKeyword(e.target.value))}
+      />
+      {notif.status && (
+        <PAlert type={notif.typeNotif} message={notif.message} />
+      )}
+      <PTableWithAction
+        status={talents.status}
+        thead={["Nama", "Role", "Avatar", "Aksi"]}
+        data={talents.data}
+        tbody={["name", "role", "avatar"]}
+        editUrl={access.edit ? `/talents/edit` : null}
+        deleteAction={access.hapus ? (id) => handleDelete(id) : null}
+        withoutPagination
+      />
+    </Container>
   );
 }
+
+export default TalentsPage;
