cafe71e3df9acd70267aa7af97a9d3221cff3ba6
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-03-06 16:16:01 +0700

    fix bug

--- src/pages/categories/edit.js
+++ src/pages/categories/edit.js
@@ -33,6 +33,7 @@ export default function PageEditCategory() {
 
   useEffect(() => {
     fetchOneCategories();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const handleSubmit = async () => {
