348766fae9cf2152e3b9929700f12693e8895a13
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-03-23 19:18:36 +0700

    make SelectBox components

---
+++ src/components/SelectBox/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Form } from "react-bootstrap";
+import Select from "react-select";
+
+export default function PSelectBox({
+  name,
+  options,
+  isClearable,
+  value,
+  placeholder,
+  handleChange,
+  label,
+}) {
+  return (
+    <Form.Group className="mb-2">
+      {label && <Form.Label>{label}</Form.Label>}
+
+      <Select
+        name={name}
+        isClearable={isClearable}
+        placeholder={placeholder}
+        options={options}
+        onChange={handleChange}
+        value={value}
+      />
+    </Form.Group>
+  );
+}
