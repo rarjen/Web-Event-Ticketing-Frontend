17fca636d5831345838f298db0f09164d9d09915
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-03-23 19:17:01 +0700

    modify index for events

--- src/pages/events/index.js
+++ src/pages/events/index.js
@@ -22,6 +22,148 @@ import {
   fetchListTalents,
 } from "../../redux/lists/actions";
 
-export default function form() {
-  return <div>form</div>;
+export default function EventPage() {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const notif = useSelector((state) => state.notif);
+  const events = useSelector((state) => state.events);
+  const lists = useSelector((state) => state.lists);
+
+  useEffect(() => {
+    dispatch(fetchEvents());
+  }, [dispatch, events.keyword, events.category, events.talent]);
+
+  useEffect(() => {
+    dispatch(fetchListTalents());
+    dispatch(fetchListCategories());
+  }, [dispatch]);
+
+  const handleDelete = (id) => {
+    Swal.fire({
+      title: "Apa kamu yakin?",
+      text: "Anda tidak akan dapat mengembalikan ini!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Iya, Hapus",
+      cancelButtonText: "Batal",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        const res = await deleteData(`/cms/events/${id}`);
+
+        dispatch(
+          setNotif(
+            true,
+            "success",
+            `Berhasil hapus speaker ${res.data.data.title}`
+          )
+        );
+
+        dispatch(fetchEvents());
+      }
+    });
+  };
+
+  const handleChangeStatus = (id, status) => {
+    Swal.fire({
+      title: "Apa kamu yakin?",
+      text: "",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Iya, Ubah Status",
+      cancelButtonText: "Batal",
+    }).then(async (result) => {
+      if (result.isConfirmed) {
+        const payload = {
+          statusEvent: status === "Published" ? "Draft" : "Published",
+        };
+
+        const res = await putData(`/cms/events/${id}/status`, payload);
+
+        dispatch(
+          setNotif(
+            true,
+            "success",
+            `Berhasil ubah status event ${res.data.data.title}`
+          )
+        );
+
+        dispatch(fetchEvents());
+      }
+    });
+  };
+
+  return (
+    <Container className="mt-3">
+      <PButton action={() => navigate("events/create")}>Tambah</PButton>
+      <PBreacCrumb textSecond={"Events"} />
+      <Row>
+        <Col>
+          <PSelectBox
+            placeholder={"Masukan pencarian kategori"}
+            name="category"
+            value={events.category}
+            options={lists.categories}
+            isClearable={true}
+            handleChange={(e) => dispatch(setCategory(e))}
+          />
+        </Col>
+        <Col>
+          <PSelectBox
+            placeholder={"Masukan pencarian pembicara"}
+            name="category"
+            value={events.talent}
+            options={lists.talents}
+            isClearable={true}
+            handleChange={(e) => dispatch(setTalent(e))}
+          />
+        </Col>
+      </Row>
+
+      {notif.status && (
+        <PAlert type={notif.typeNotif} message={notif.message} />
+      )}
+
+      <PTableWithActions
+        status={events.status}
+        thead={[
+          "Judul",
+          "Tanggal",
+          "Tempat",
+          "Status",
+          "Kategori",
+          "Pembicara",
+          "Aksi",
+        ]}
+        data={events.data}
+        tbody={[
+          "title",
+          "date",
+          "venueName",
+          "statusEvent",
+          "categoryName",
+          "talentName",
+        ]}
+        editUrl={`/events/edit`}
+        deleteAction={(id) => handleDelete(id)}
+        customAction={(id, status = "") => {
+          return (
+            <PButton
+              className={"mx-2"}
+              variant="primary"
+              size={"sm"}
+              action={() => handleChangeStatus(id, status)}
+            >
+              Change Status
+            </PButton>
+          );
+        }}
+        withoutPagination
+      />
+    </Container>
+  );
 }
