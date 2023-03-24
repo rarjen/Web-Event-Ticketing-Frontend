c95ccffe53e509ee3e23c5ea32c851413693861e
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-03-24 11:07:58 +0700

    import route orders

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,6 +8,7 @@ import { CategoriesRoute } from "./CategoriesRoute";
 import { TalentsRoute } from "./TalentsRoute";
 import { PaymentsRoute } from "./PaymentsRoute";
 import { EventsRoute } from "./EventsRoute";
+import { OrdersRoute } from "./OrdersRoute";
 import PNavbar from "../components/Navbar";
 
 export function AppRoutes() {
@@ -36,6 +37,7 @@ export function AppRoutes() {
         <Route path="talents/*" element={<TalentsRoute />} />
         <Route path="payments/*" element={<PaymentsRoute />} />
         <Route path="events/*" element={<EventsRoute />} />
+        <Route path="orders/*" element={<OrdersRoute />} />
         <Route path="/" element={<Navigate to="/dashboard" replace={true} />} />
       </Route>
     </Routes>
