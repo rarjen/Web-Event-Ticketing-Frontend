089a4dc8bfe36258055055b2c4492c76eaaf9c21
Otniel Kevin Abiel <rarjen57@gmail.com>
2023-04-14 10:14:43 +0700

    fix params

--- src/routes/EventsRoute.js
+++ src/routes/EventsRoute.js
@@ -9,7 +9,7 @@ export function EventsRoute() {
     <Routes>
       <Route path="/" element={<Events />} />
       <Route path="/create" element={<Create />} />
-      <Route path="/edit/:eventsId" element={<Edit />} />
+      <Route path="/edit/:eventId" element={<Edit />} />
     </Routes>
   );
 }
